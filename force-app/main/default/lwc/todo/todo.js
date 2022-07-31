2637eb344952ea64ad6f04af577780a8be106082
Karan Singh Solanki <karan.solanki@metacube.com>
2022-07-31 16:07:28 +0530

    Implemented the addTasks functionality to dynamically add task

--- force-app/main/default/lwc/todo/todo.js
+++ force-app/main/default/lwc/todo/todo.js
@@ -1,3 +1,39 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Todo extends LightningElement {}
+export default class Todo extends LightningElement {
+	@track state = {
+		newTaskName: '',
+		tasks: [],
+	};
+
+	handleTaskInput(event) {
+		this.newTaskName = event.detail.value; // this.newTaskName is calling the set newTaskName(value)
+	}
+
+	handleAddNewTask() {
+		this.tasks.push(this.createTask());
+		this.newTaskName = '';
+	}
+
+	createTask() {
+		return {
+			Id: this.tasks.length,
+			Name: this.newTaskName,
+		};
+	}
+
+	get newTaskName() {
+		return this.state.newTaskName;
+	}
+	set newTaskName(value) {
+		this.state.newTaskName = value;
+	}
+
+	get tasks() {
+		return this.state.tasks;
+	}
+
+	get haveTasks() {
+		return this.tasks && this.tasks.length > 0;
+	}
+}
